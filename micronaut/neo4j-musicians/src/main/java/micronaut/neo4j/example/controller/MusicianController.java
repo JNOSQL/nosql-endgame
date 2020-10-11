97cba9ba56eaa4a1095ed652390fb062511f27cb
Werner Keil <werner.keil@gmx.net>
2020-10-11 23:22:33 +0200

    Removed uneeded import

--- micronaut/neo4j-musicians/src/main/java/micronaut/neo4j/example/controller/MusicianController.java
+++ micronaut/neo4j-musicians/src/main/java/micronaut/neo4j/example/controller/MusicianController.java
@@ -3,13 +3,10 @@ package micronaut.neo4j.example.controller;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
-import io.micronaut.http.annotation.Put;
 import micronaut.neo4j.example.controller.dto.MusicianDTO;
 import micronaut.neo4j.example.service.MusicianService;
 
-import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
-import java.util.List;
 
 @Controller("/api/musician")
 public class MusicianController {
