d034058312daff01a9c00c667060baea36f38e6c
Werner Keil <werner.keil@gmx.net>
2020-10-18 07:17:30 +0200

    Added Column demos

---
+++ jakarta/hazelcast/src/main/java/org/jnosql/demo/endgame/jakarta/hazelcast/UserRepoApp.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017 Otávio Santana and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
+ * and the Apache License v2.0 is available at http://www.opensource.org/licenses/apache2.0.php.
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ *
+ * Contributors:
+ *
+ * Otavio Santana
+ * Werner Keil
+ */
+
+package org.jnosql.demo.endgame.jakarta.hazelcast;
+
+
+import org.eclipse.jnosql.artemis.DatabaseQualifier;
+import org.jnosql.demo.endgame.jakarta.hazelcast.model.User;
+import org.jnosql.demo.endgame.jakarta.hazelcast.repository.UserRepository;
+
+import javax.enterprise.inject.se.SeContainer;
+import javax.enterprise.inject.se.SeContainerInitializer;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class UserRepoApp {
+
+    private static final User USER = User.builder().
+            withPhones(Arrays.asList("234", "432"))
+            .withUsername("username")
+            .withName("Name")
+            .build();
+
+    public static void main(String[] args) {
+
+        try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
+
+            UserRepository repository = container.select(UserRepository.class, DatabaseQualifier.ofKeyValue()).get();
+            repository.save(USER);
+            Optional<User> user = repository.findById("username");
+            System.out.println("User found: " + user);
+            System.out.println("Entity found: " + repository.existsById("username"));
+            
+            System.exit(0);
+        }
+    }
+
+    private UserRepoApp() {
+    }
+}
---
+++ jakarta/redis/README.md
@@ -0,0 +1,27 @@
+# Key-Value - Redis
+
+![Redis Project](https://jnosql.github.io/img/logos/redis.png)
+
+**Redis**: Redis is a software project that implements data structure servers. It is open-source, networked, in-memory, and stores keys with optional durability.
+
+To run this project a Redis instance is required, so you can use either a local installation or use Docker.
+
+## Manual installation
+Follow the instructions in https://redis.io/download
+
+## Docker
+![Docker](https://www.docker.com/sites/default/files/horizontal_large.png)
+
+
+1. Install docker: https://www.docker.com/
+1. https://hub.docker.com/_/redis/
+1. Run docker command
+1. `docker run --name redis-instance -p 6379:6379 -d redis`
+
+
+
+## Run the code
+
+With a Redis instance running go to the class **App** and have fun.
+
+
