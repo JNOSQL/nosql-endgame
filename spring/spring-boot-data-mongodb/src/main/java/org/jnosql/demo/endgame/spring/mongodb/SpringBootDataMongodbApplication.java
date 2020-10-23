7d98f18b2ffc9b87894ef7f0c52abffed412643d
Tegridy <jarjak4@gmail.com>
2020-10-23 17:19:00 +0200

    Added Spring Boot MongoDB sample data initialization

--- spring/spring-boot-data-mongodb/src/main/java/org/jnosql/demo/endgame/spring/mongodb/SpringBootDataMongodbApplication.java
+++ spring/spring-boot-data-mongodb/src/main/java/org/jnosql/demo/endgame/spring/mongodb/SpringBootDataMongodbApplication.java
@@ -1,12 +1,29 @@
 package org.jnosql.demo.endgame.spring.mongodb;
 
+import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.SpringDataMongo3Driver;
+import com.github.cloudyrock.spring.v5.MongockSpring5;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @SpringBootApplication
 public class SpringBootDataMongodbApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootDataMongodbApplication.class, args);
+    }
+
+    @Bean
+    public MongockSpring5.MongockInitializingBeanRunner mongockInitializingBeanRunner(
+            ApplicationContext springContext,
+            MongoTemplate mongoTemplate){
+        return MongockSpring5.builder()
+                .setDriver(SpringDataMongo3Driver
+                        .withDefaultLock(mongoTemplate))
+                .addChangeLogsScanPackage("org.jnosql.demo.endgame.spring.mongodb.changelogs")
+                .setSpringContext(springContext)
+                .buildInitializingBeanRunner();
     }
 }
